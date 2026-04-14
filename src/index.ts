f25854fa7e154bbeda50f0cf0ddae1a5a2e1a058
OKFred <67962064+OKFred@users.noreply.github.com>
2026-04-14 21:54:55 +0800

    实测-以ebay为例

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import {
+  AndroidAgent,
+  AndroidDevice,
+  getConnectedDevices,
+} from "@midscene/android";
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+Promise.resolve(
+  (async () => {
+    const devices = await getConnectedDevices();
+    const device = new AndroidDevice(devices[0].udid);
+    console.table(device);
+    const agent = new AndroidAgent(device, {
+      aiActionContext:
+        "如果出现任何位置信息、权限、用户协议等弹窗，请点击同意。如果弹出登录页面，请关闭它。",
+    });
+    await device.connect();
+
+    await agent.aiAct("打开浏览器并访问ebay.com");
+    await sleep(5000);
+    await agent.aiAct('在搜索框中输入"Headphones"，并回车');
+    await agent.aiWaitFor("页面上至少出现一个Headphones商品");
+
+    const items = await agent.aiQuery(
+      "{itemTitle: string, price: Number}[]，找出列表中的商品以及对应的价格",
+    );
+    console.log("headphones in stock", items);
+    let question = "页面左侧有商品分类过滤选项";
+    let result: "YES" | "NO" | null = null;
+    try {
+      await agent.aiAssert(question);
+      result = "YES";
+    } catch (error) {
+      result = "NO";
+    }
+    console.log("result: ", question, result);
+  })(),
+);
